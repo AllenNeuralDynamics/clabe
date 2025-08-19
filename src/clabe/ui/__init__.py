f69fdeb008ee1673c8c164bd1647930fc3a2d91e
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-08-19 12:59:46 -0700

    Remove base picker

--- src/clabe/ui/__init__.py
+++ src/clabe/ui/__init__.py
@@ -1,4 +1,3 @@
-from .picker import PickerBase
 from .ui_helper import DefaultUIHelper, UiHelper, prompt_field_from_input
 
-__all__ = ["PickerBase", "DefaultUIHelper", "UiHelper", "prompt_field_from_input"]
+__all__ = ["DefaultUIHelper", "UiHelper", "prompt_field_from_input"]
