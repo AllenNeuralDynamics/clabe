dbdd8e1a5bcccc6524843041e14d9441071ba17d
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2025-10-01 12:18:09 -0700

    Refactor package metadata to use uv as a version management

--- src/clabe/__init__.py
+++ src/clabe/__init__.py
@@ -1,14 +1,6 @@
-__version__ = "0.6.14"
+from importlib.metadata import PackageNotFoundError, version
 
-import logging
-
-from . import logging_helper
-
-logger = logging.getLogger(__name__)
-
-logging.basicConfig(
-    level=logging.INFO,
-    format=logging_helper.log_fmt,
-    datefmt=logging_helper.datetime_fmt,
-    handlers=[logging_helper.rich_handler],
-)
+try:
+    __version__ = version("aind-clabe")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
